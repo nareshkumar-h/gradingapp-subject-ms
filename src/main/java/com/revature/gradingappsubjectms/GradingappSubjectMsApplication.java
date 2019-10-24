29642b272877f734c5c04b6feda790cf4a1e8440
Priyanka-Sri <priyankavallan976@gmail.com>
2019-10-24 14:12:21 +0530

    added

---
+++ src/main/java/com/revature/gradingappsubjectms/GradingappSubjectMsApplication.java
@@ -0,0 +1,13 @@
+package com.revature.gradingappsubjectms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradingappSubjectMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradingappSubjectMsApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/db/migration/V1__Create_Subject.sql
@@ -0,0 +1,10 @@
+
+
+create table subject(
+       id int primary key auto_increment,
+       subcode varchar(10) unique not null,
+       name varchar(10) unique not null);
+
+
+
+
